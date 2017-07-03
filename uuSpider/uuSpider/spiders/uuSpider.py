c934eeaf1b59a2594e2aa3bf18c31f39055f4acc
jeikerxiao <jeiker@126.com>
2017-07-03 14:49:14 +0800

    optimized code uuSpider.py
    optimized code uuSpider.py

--- uuSpider/uuSpider/spiders/uuSpider.py
+++ uuSpider/uuSpider/spiders/uuSpider.py
@@ -1,5 +1,5 @@
+# -*- coding: utf-8 -*-
 import scrapy
-from lxml import etree
 from scrapy.http import Request
 from uuSpider.items import UuspiderItem
 
@@ -16,12 +16,12 @@ class QuanjiSpider(scrapy.Spider):
     def start_requests(self):
         return [Request("http://www.uu.com/logging.php?action=login", callback=self.post_login)]
 
-    # FormRequeset
+    # 登录表单提交
     def post_login(self, response):
-        print('Preparing login')
+        self.logger.info('---- login start ----')
         # 下面这句代码用于抓取请求网页后返回网页中的 formhash 字段的值, 用于成功提交表单
         formhash = response.xpath('//input[@name="formhash"]/@value').extract()[0]
-        print(formhash)
+        self.logger.info('formhash: ' + formhash)
         # FormRequeset.from_response是Scrapy提供的一个函数, 用于post表单
         # 登陆成功后, 会调用after_login回调函数
         return [scrapy.FormRequest.from_response(response,
@@ -39,27 +39,26 @@ class QuanjiSpider(scrapy.Spider):
 
     # 登录成功后，进入列表分页
     def after_login(self, response):
-        print('login success')
-        for id in range(1, 41, 1):
-            url = 'http://www.uu.com/forumdisplay.php?fid=5&page=' + str(id)
+        self.logger.info('---- login success -----')
+        for page_num in range(1, 41, 1):
+            url = 'http://www.uu.com/forumdisplay.php?fid=5&page=' + str(page_num)
             yield Request(url, headers=self.headers, callback=self.parse)
 
     # 解析列表分页上的标题、日期、内容页跳转连接
     def parse(self, response):
         # 将request.content 转化为 Element
-        root = etree.HTML(response.text)
-        items = root.xpath('//form[@name="moderate"]/*/div[@class="spaceborder"]/table/tr')
+        items = response.xpath('//form[@name="moderate"]/*/div[@class="spaceborder"]/table/tr')
         for item in items:
-            content_url = 'http://www.uu.com/'+item.xpath('./td[@class="f_title"]/a/@href')[0]
+            url_str = 'http://www.uu.com/'+item.xpath('./td[@class="f_title"]/a/@href').extract()[0]
             title_str = ''
             date_str = ''
             try:
-                title_str = item.xpath('./td[@class="f_title"]/a/text()')
-                date_str = item.xpath('./td[@class="f_last"]/span/a/text()')
+                title_str = item.xpath('./td[@class="f_title"]/a/text()').extract()[0]
+                date_str = item.xpath('./td[@class="f_last"]/span/a/text()').extract()[0]
             except:
-                print('faild!')
+                self.logger.error('get list page failure!')
                 pass
-            yield Request(content_url, headers=self.headers, callback=self.parseImage, meta={'title': title_str,
+            yield Request(url_str, headers=self.headers, callback=self.parseImage, meta={'title': title_str,
                                                                                              'date': date_str})
 
     # 解析内容页上的 图片url , 传入item。
@@ -67,15 +66,14 @@ class QuanjiSpider(scrapy.Spider):
         title = response.meta['title']
         date = response.meta['date']
         # 将request.content 转化为 Element
-        root = etree.HTML(response.text)
-        items = root.xpath('//div[@class="t_msgfont"]/img')
+        items = response.xpath('//div[@class="t_msgfont"]/img')
         save_item = UuspiderItem()
         for item in items:
             try:
-                save_item['title'] = title[0]
-                save_item['date'] = date[0]
-                save_item['image_urls'] = item.xpath('./@src')
+                save_item['title'] = title
+                save_item['date'] = date
+                save_item['image_urls'] = item.xpath('./@src').extract()
                 yield save_item
             except:
-                print('faild!')
+                self.logger.error('get content page failure!')
                 pass
