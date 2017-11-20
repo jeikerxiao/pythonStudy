38fb25298d09f22ae198d3a2854608a35c1e147d
jeikerxiao <jeiker@126.com>
2017-11-20 09:24:02 +0800

    change bookSpider
    change bookSpider

--- uuSpider/uuSpider/spiders/uuSpider.py
+++ uuSpider/uuSpider/spiders/uuSpider.py
@@ -7,14 +7,14 @@ from uuSpider.items import UuspiderItem
 class QuanjiSpider(scrapy.Spider):
 
     name = 'uuSpider'
-    allowed_domains = ['uu.com']
+    allowed_domains = ['mayattt.com']
     headers = {
         "Accept-Language": "zh-CN,zh;q=0.8,en;q=0.6",
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
     }
 
     def start_requests(self):
-        return [Request("http://www.uu.com/logging.php?action=login", callback=self.post_login)]
+        return [Request("http://www.mayattt.com/logging.php?action=login", callback=self.post_login)]
 
     # 登录表单提交
     def post_login(self, response):
@@ -27,10 +27,10 @@ class QuanjiSpider(scrapy.Spider):
         return [scrapy.FormRequest.from_response(response,
                                           formdata={
                                               'formhash': formhash,
-                                              'referer': 'http://www.uu.com/index.php',
+                                              'referer': 'http://www.mayattt.com/index.php',
                                               'loginfield': 'username',
-                                              'username': '账号',
-                                              'password': '密码',
+                                              'username': 'mayajeiker',
+                                              'password': 'friendship',
                                               'questionid': '0',
                                               'cookietime': '12592000',
                                           },
@@ -41,7 +41,7 @@ class QuanjiSpider(scrapy.Spider):
     def after_login(self, response):
         self.logger.info('---- login success -----')
         for page_num in range(1, 41, 1):
-            url = 'http://www.uu.com/forumdisplay.php?fid=5&page=' + str(page_num)
+            url = 'http://www.mayattt.com/forumdisplay.php?fid=5&page=' + str(page_num)
             yield Request(url, headers=self.headers, callback=self.parse)
 
     # 解析列表分页上的标题、日期、内容页跳转连接
@@ -49,7 +49,7 @@ class QuanjiSpider(scrapy.Spider):
         # 将request.content 转化为 Element
         items = response.xpath('//form[@name="moderate"]/*/div[@class="spaceborder"]/table/tr')
         for item in items:
-            url_str = 'http://www.uu.com/'+item.xpath('./td[@class="f_title"]/a/@href').extract()[0]
+            url_str = 'http://www.mayattt.com/'+item.xpath('./td[@class="f_title"]/a/@href').extract()[0]
             title_str = ''
             date_str = ''
             try:
